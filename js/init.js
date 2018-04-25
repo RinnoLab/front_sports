d45ae637e478ce31aa45f3857c2d542cb55635c5
Axel Díaz <diaz.axelio@gmail.com>
2018-04-25 14:25:55 -0300

    Añadiendo función Pushpin a los menúes

--- js/init.js
+++ js/init.js
@@ -12,4 +12,16 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $('.tabs').tabs();
+});
+
+$(document).ready(function(){
+  $('.pushpin').pushpin();
+});
+$('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
   });
